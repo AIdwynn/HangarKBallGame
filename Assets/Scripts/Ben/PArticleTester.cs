43dc94062babd6998effde31a98c4f6981153f69
BendrGames <vanhaelst.ben@hotmail.com>
2022-07-05 16:12:23 +0200

    particlesystem

--- Assets/Scripts/Ben/PArticleTester.cs
+++ Assets/Scripts/Ben/PArticleTester.cs
@@ -2,66 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteAlways]
 
-public class PArticleTester : MonoBehaviour
+
+public class ParticleTester : MonoBehaviour
 {
-    public Transform secondlocation;
+    public Transform secondlocation; 
 
-    public List<Transform> randomColours;
+    float timer = 0f;
 
-    public bool PlayConstant = false;
-
-    public bool stopConstant = false;
-
-
-    public bool PlayDirectional = false;
-
-    public bool PlaySpreading = false;
-
-    private void Start()
-    {
-      
-    }
 
     private void Update()
     {
-        ConstantParticle();
+        timer += Time.deltaTime;
 
-        if (PlayDirectional)
+        if (timer >= 2)
         {
-            ParticleSpawner.Instance.SetParticleColourBasedOnCollision(ParticleToPlay.Directional, randomColours[Random.Range(0, randomColours.Count)]);
-            ParticleSpawner.Instance.SetDirectionalParticleSize(0.5f, 1.5f);
-            ParticleSpawner.Instance.SetDirectionalParticleSpeed(2f, 5f);
-            ParticleSpawner.Instance.PlayDirectionalParticles(transform, Random.Range(0, 360));
-            PlayDirectional = false;
+
+            new ParticleSpawner(ParticleType.Directional)
+                          .ChangeSpeed(2f, 2.6f)
+                          .ChangeSize(0.1f, 0.6f)
+                          .SetColour(Color.black)
+                          .Rotate(Random.Range(0, 360))
+                          .Activate(transform);
+
+            new ParticleSpawner(ParticleType.Directional)
+                               .ChangeSpeed(1f, 1.5f)
+                               .ChangeSize(0.5f, 1f)
+                               .Rotate(Random.Range(0, 360))
+                               .Activate(secondlocation);
+
+
+            timer = 0;
         }
 
-        if (PlaySpreading)
-        {
-            ParticleSpawner.Instance.SetParticleColourBasedOnCollision(ParticleToPlay.Spreading, randomColours[Random.Range(0, randomColours.Count)]);
-            ParticleSpawner.Instance.SetSpreadingParticleSpeed(0.5f, 1.5f);
-            ParticleSpawner.Instance.SetSpreadingParticleSize(0.5f, 1.5f);
-            ParticleSpawner.Instance.PlaySpreadingParticles(transform);
-            PlaySpreading = false;
-        }
     }
 
-    private void ConstantParticle()
-    {
-        if (PlayConstant)
-        {
-            ParticleSpawner.Instance.PlayConstantParticle(transform);
-            
-            PlayConstant = false;
-        }
-        if (stopConstant)
-        {
-            ParticleSpawner.Instance.StopConstantParticle();
-            
-            stopConstant = false;
-        }
-        //ParticleSpawner.Instance.SetParticleColourBasedOnCollision(ParticleToPlay.Constant, randomColours[Random.Range(0, randomColours.Count - 1)]);
-        ParticleSpawner.Instance.MoveConstantParticle(transform);
-    }
+
 }
+
+
+
--- Assets/Scripts/Ben/ParticleSpawner.cs
+++ Assets/Scripts/Ben/ParticleSpawner.cs
@@ -1,153 +1,81 @@
-using DAE.Gamesystem.Singleton;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ParticleSpawner : SingletonMonoBehaviour<ParticleSpawner>
+public class ParticleSpawner
 {
-    [Header("animation")]
-    [SerializeField] private ParticleSystem _directionalParticles, _constantParticles, _spreadingPArticles;
 
+    private ParticleSystem _currentSystem;
+    private ParticleType _particleType;
 
-    private ParticleSystem.MinMaxGradient _currentGradient;
-
-
-
-    //setColour of Particle
-    public void SetParticleColourBasedOnCollision(ParticleToPlay particleType, Transform objectToChangeColourToo)
+    public ParticleSpawner(ParticleType particleType)
     {
-        if (objectToChangeColourToo != null)
+        _currentSystem = ParticleObjectPool.Instance.GetParticleSystem(particleType);
+        _particleType = particleType;
+    }
+
+    public ParticleSpawner Activate(Transform PlayLocation)
+    {
+        if (_particleType == ParticleType.Constant)
         {
-            if (objectToChangeColourToo.transform.TryGetComponent(out SpriteRenderer r))
-            {
-                _currentGradient = new ParticleSystem.MinMaxGradient(r.color * 0.9f, r.color * 1.2f);
-
-                switch (particleType)
-                {
-                    case ParticleToPlay.Directional:
-                        SetColor(_directionalParticles);
-                        break;
-                    case ParticleToPlay.Constant:
-                        SetColor(_constantParticles);
-                        break;
-                    case ParticleToPlay.Spreading:
-                        SetColor(_spreadingPArticles);
-                        break;
-                    default:
-                        break;
-                }                
-            }
+            ParticleObjectPool.Instance.PlayConstantParticleSystem(_currentSystem, PlayLocation);
+            return this;
         }
+        ParticleObjectPool.Instance.PlayOneShotParticleSystem(_currentSystem, PlayLocation);
+
+        return this;
+    }
+
+    public ParticleSpawner StopConstantAfterSeconds(float time)
+    {
         
-    }
+        ParticleObjectPool.Instance.StopConstantParticleSystemAfterSeconds(_currentSystem, time);
 
-    //play burst playparticles
-    public void PlayDirectionalParticles(Transform PlayLocation, float ZRotationOfCollosion)
+        return this;
+    }   
+
+    public ParticleSpawner StopConstant()
     {
-        _directionalParticles.transform.rotation = Quaternion.Euler(0, 0, ZRotationOfCollosion);
-        _directionalParticles.transform.position = PlayLocation.position;    
-        _directionalParticles.Play();
+        ParticleObjectPool.Instance.StopConstantParticleSystem(_currentSystem);
+
+        return this;
     }
 
-    public void SetDirectionalParticleSpeed(float minSpeed, float maxSpeed)
+    public ParticleSpawner SetParticleColourBasedOnObject(Transform objectToChangeColourToo)
     {
-        var main = _directionalParticles.main;
+        ParticleObjectPool.Instance.SetParticleColourBasedOnCollision(_currentSystem, objectToChangeColourToo);
 
-        var mult = Random.Range(minSpeed, maxSpeed);
-
-        main.startSpeedMultiplier = mult;
-        main.startLifetimeMultiplier = mult;
+        return this;
     }
 
-    public void SetDirectionalParticleSize(float minSize, float maxSize)
+    public ParticleSpawner SetColour(Color color)
     {
-        var main = _directionalParticles.main;
+        ParticleObjectPool.Instance.SetColor(_currentSystem, color);
 
-        var mult = Random.Range(minSize, maxSize);
-
-        main.startSizeMultiplier = mult;
+        return this;
     }
 
-
-    //must be played in update
-    public void PlayConstantParticle(Transform PlayLocation)
+    public ParticleSpawner Rotate(float Zrotation)
     {
-        _constantParticles.transform.position = PlayLocation.position;
-        _constantParticles.Play();
+        ParticleObjectPool.Instance.RotateParticleSystemZ(_currentSystem, Zrotation);
+
+        return this;
     }
 
-    public void SetconstantParticleSpeed(float minSpeed, float maxSpeed)
+    public ParticleSpawner ChangeSpeed(float minSpeed, float maxSpeed)
     {
-        var main = _constantParticles.main;
+        ParticleObjectPool.Instance.SetDirectionalParticleSpeed(_currentSystem, minSpeed, maxSpeed);
 
-        var mult = Random.Range(minSpeed, maxSpeed);
-
-        main.startSpeedMultiplier = mult;
-        main.startLifetimeMultiplier = mult;
+        return this;
     }
 
-    public void SetconstantParticleSize(float minSize, float maxSize)
+    public ParticleSpawner ChangeSize(float minSize, float maxSzie)
     {
-        var main = _constantParticles.main;
+        ParticleObjectPool.Instance.SetDirectionalParticleSize(_currentSystem, minSize, maxSzie);
 
-        var mult = Random.Range(minSize, maxSize);
-
-        main.startSizeMultiplier = mult;
-        
-    }
-
-
-    public void MoveConstantParticle(Transform PlayLocation)
-    {
-        _constantParticles.transform.position = PlayLocation.position;
-    }
-
-    public void StopConstantParticle()
-    {
-        _constantParticles.Stop();
-    }
-
-
-    public void PlaySpreadingParticles(Transform PlayLocation)
-    {
-        _spreadingPArticles.transform.position = PlayLocation.position;
-        _spreadingPArticles.Play();
-    }
-
-    public void SetSpreadingParticleSpeed(float minSpeed, float maxSpeed)
-    {
-        var main = _spreadingPArticles.main;
-
-        var mult = Random.Range(minSpeed, maxSpeed);
-
-        main.startSpeedMultiplier = mult;
-        main.startLifetimeMultiplier = mult;
-    }
-
-    public void SetSpreadingParticleSize(float minSize, float maxSize)
-    {
-        var main = _spreadingPArticles.main;
-
-        var mult = Random.Range(minSize, maxSize);
-
-        main.startSizeMultiplier = mult;
-
+        return this;
     }
 
 
 
-
-
-    void SetColor(ParticleSystem ps)
-    {
-        var main = ps.main;
-        main.startColor = _currentGradient;
-    }
-}
-
-public enum ParticleToPlay
-{
-    Directional,
-    Constant,
-    Spreading
 }
